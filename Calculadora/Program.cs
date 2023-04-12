900e2afcf1a37800eb4383454504f021da833d52
Daniel Alejandro Fernandez <daf111@yahoo.com.ar>
2023-04-11 21:18:11 -0300

    Initial commit

---
+++ Calculadora/Program.cs
@@ -0,0 +1,5 @@
+﻿using Calculadora;
+
+Sumador sumador = new Sumador();
+Console.WriteLine(sumador.sumar(1, 1));
+Console.WriteLine(sumador.sumar(2, 2));
---
+++ Calculadora/Sumador.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class Sumador
+    {
+        public int sumar(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
