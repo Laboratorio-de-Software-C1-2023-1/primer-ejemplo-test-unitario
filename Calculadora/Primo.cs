6de6dc054f0ae32f852a5059f211c6c0f1b2a156
Daniel Fernandez <daf111@yahoo.com.ar>
2023-04-18 20:15:31 -0300

    Implement Prime feature

---
+++ Calculadora/Primo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class Primo
+    {
+        public bool EsPrimo(int numero)
+        {
+            if (numero == 1)
+            {
+                return false;
+            }
+
+            this.EsMayorA1000(numero);
+
+            for (int i = 2; i < numero; i++)
+            {
+                if (numero % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private void EsMayorA1000(int numero)
+        {
+            if (numero > 1000)
+            {
+                throw new ArgumentException("No se permite calcular numeros mayores a 1000");
+            }
+        }
+    }
+}
---
+++ CalculadoraTest/PrimoTest.cs
@@ -0,0 +1,93 @@
+﻿using Calculadora;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CalculadoraTest
+{
+    public class PrimoTest
+    {
+
+        [Fact]
+        public void Primo_ConValorNoPrimo_DevuelveNoEsPrimo()
+        {
+            //Arrange
+            Primo primo = new Primo();
+            bool resultadoEsperado = false;
+
+            //Act
+            bool resultadoReal = primo.EsPrimo(4);
+
+            //Assert
+            Assert.Equal(resultadoEsperado, resultadoReal);
+        }
+
+        [Fact]
+        public void Primo_ConValorUno_DevuelveNoEsPrimo()
+        {
+            //Arrange
+            Primo primo = new Primo();
+            bool resultadoEsperado = false;
+
+            //Act
+            bool resultadoReal = primo.EsPrimo(1);
+
+            //Assert
+            Assert.Equal(resultadoEsperado, resultadoReal);
+        }
+
+        [Fact]
+        public void Primo_ConValorDos_DevuelveEsPrimo()
+        {
+            //Arrange
+            Primo primo = new Primo();
+            bool resultadoEsperado = true;
+
+            //Act
+            bool resultadoReal = primo.EsPrimo(2);
+
+            //Assert
+            Assert.Equal(resultadoEsperado, resultadoReal);
+        }
+
+
+
+        [Fact]
+        public void Primo_ConValorPrimo_DevuelveEsPrimo()
+        {
+            //Arrange
+            Primo primo = new Primo();
+            bool resultadoEsperado = true;
+
+            //Act
+            bool resultadoReal = primo.EsPrimo(7);
+
+            //Assert
+            Assert.Equal(resultadoEsperado, resultadoReal);
+        }
+
+
+        [Fact]
+        public void Primo_ConValorInvalido_ArrojaException()
+        {
+            //Arrange
+            Primo primo = new Primo();
+
+            //Act & Assert
+            try
+            {
+                bool resultadoReal = primo.EsPrimo(1001);
+            } catch (ArgumentException ex)
+            {
+                Assert.Equal("No se permite calcular numeros mayores a 1000", ex.Message);
+                return;
+            }
+
+            Assert.True(false, "No devolvió excepción esperada");
+        }
+
+    }
+}
